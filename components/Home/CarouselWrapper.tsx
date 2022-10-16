ae87b97a316da11e031cc0710acbada08326731a
Mauricio Sanchez <mabeto97@hotmail.com>
2022-10-16 16:22:23 -0500

    Adding a new image in the Carousel and updating the Locations Google Maps links with the correct branding of Open24

--- components/Home/CarouselWrapper.tsx
+++ components/Home/CarouselWrapper.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import image1 from "../../assets/image1.png";
 import image2 from "../../assets/image2.png";
 import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
 
 export const CarouselWrapper = () => {
   const [index, setIndex] = useState(0);
@@ -16,41 +17,45 @@ export const CarouselWrapper = () => {
     setIndex(selectedIndex);
   };
 
+  const carouselImages = [{
+    alt : 'First slide',
+    width : "1920px",
+    height : "1080px",
+    src : image1
+  },{
+    alt : 'Second slide',
+    width : "1920px",
+    height : "1080px",
+    src : image2
+  },
+  {
+    alt : 'Third slide',
+    width : "1920px",
+    height : "1080px",
+    src : image3
+  },
+  {
+    alt : 'Fourth slide',
+    width : "1920px",
+    height : "1080px",
+    src : image4
+  }]
+
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item interval={6000}>
-        {/*<img
-          className="d-block w-100"
-          src="https://fondosmil.com/fondo/11771.jpg"
-         src="https://fondosmil.com/fondo/11771.jpg"
-          alt="First slide"
-        />*/}
-        <Image
-          src={image1}
-          className="d-block w-100"
-          alt="First slide"
-          width="1920px"
-          height="1080px"
-        />
-      </Carousel.Item>
-      <Carousel.Item interval={6000}>
-        <Image
-          className="d-block w-100"
-          src={image2}
-          alt="Second slide"
-          width="1920px"
-          height="1080px"
-        />
-      </Carousel.Item>
-      <Carousel.Item interval={6000}>
-      <Image
-          className="d-block w-100"
-          src={image3}
-          alt="Third slide"
-          width="1920px"
-          height="1080px"
-        />
-      </Carousel.Item>
+      {carouselImages.map(({alt,width,height,src})=>{
+        return(
+          <Carousel.Item interval={6000}>
+          <Image
+            src={src}
+            className="d-block w-100"
+            alt={alt}
+            width={width}
+            height={height}
+          />
+        </Carousel.Item>
+        )
+      })}
     </Carousel>
   );
 };
